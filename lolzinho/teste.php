d6695d2d02c8d1f29c00462b3b3542ebf6bd0bdb
jobsonjose <jjortb@gmail.com>
2017-03-21 16:58:34 -0300

    Iniciando Projeto 1.0
    Vamos começar

---
+++ lolzinho/teste.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>conta feita</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="lol.css">
+</head>
+<body>
+		<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="Home.html"><img src="icone2.png" width="100" height="73"></a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="#" class="active">Time</a></li>
+            <li><a href="#">Rank</a></li>
+            <li><a href="#" >Tutorial</a></li>
+            <li><a href="#" >Suporte</a></li>
+        </ul>
+        <form class="navbar-form navbar-left" method="GET">
+        <div class="input-group">
+        <input type="text" class="form-control" placeholder="Busca">
+        <div class="input-group-btn">
+            <button type="submit" class="btn btn-default">
+                <i class="glyphicon glyphicon-search"></i>
+            </button>
+        </div>
+        </div>
+        </form>
+    </div>
+</nav>
+
+    <center><div id="form">
+        <label>Sucesso Você concluiu o Cadastro!!</label>
+        <br>
+        <br>
+        <center><label>Obrigador!!</label></center>
+        <footer><center><p>ProjetoLolzin @2017.net</p></center></footer>
+    </div></center>
+
+</body>
+</html>
